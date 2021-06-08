f214ffbd29276582b8258a32962d9c779ee60461
¨dayana <dayana_3@hotmail.com>
2021-06-08 20:19:34 -0300

    ¨¨

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.contenedor-portafolio', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.contenedor-nosotros', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.contenedor-testimonios', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+
+
+
+
+
